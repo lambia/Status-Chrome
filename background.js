920b52d971bf57991ab362dbac6f6afe0f02ce3b
Luca <luca@lambia.it>
2017-09-07 09:35:56 +0200

    Minor changes
    Fixed some little bugs, added notification toast and shortcut.

--- background.js
+++ background.js
@@ -1,20 +1,47 @@
 /*
 TODO:
-ctrl+tab o click utente apre
+ctrl+tab o click utente apre (come distinguere?)
 isActive in whichTab, to lock it on one just one tab
-test the bottom code for js injection
-todo: shortcut per toggle
-bugfix: icona quando ctrl-tabbi
+fallback for shortcut already assigned (alert, new one)
+fallback for notifications (if !permissions or !compatibility, go with alert)
+notification onclick -> clear(id)
+test the background.dev.js code for js injection in page
+mute and wait for complete load of the tab before closing it
+*/
+
+/*
+CHANGELOG:
+Shortcut to toggle protection (ALT + SHIFT + S)
+Removed trash code (comments, dev stuff, old test)
+Fixed bug for the icon change just on the active tab
+Added notification toast instead of alert
 */
 
 isActive = false;
 whichTab = false;
 
-//On browserAction click..
-chrome.browserAction.onClicked.addListener(function(tab) {
+var alertOn = { type: "basic", iconUrl: "icons/warning.png", title: "Protection is ON",
+    message: "Opening new windows will now be prevented. \nYou can't as well: script too raw to distinguish." };
+var alertOff = { type: "basic", iconUrl: "icons/circle.www.png", title: "Protection is OFF",
+    message: "Spam is welcome!" };
+
+//Activate/deactivate the protection
+function toggleMe(){
     isActive = !isActive;
-    isActive ? chrome.browserAction.setIcon({path: "icons/warning.png", tabId:tab.id}) : chrome.browserAction.setIcon({path: "icons/circle.www.png", tabId:tab.id});
-    isActive ? alert("Window opening is now blocked!") : alert("Back to normal life.");
+    isActive ? chrome.browserAction.setIcon({path: "icons/warning.png"}) : chrome.browserAction.setIcon({path: "icons/circle.www.png"});
+    chrome.notifications.create("", isActive ? alertOn : alertOff, function(id) {
+        timer = setTimeout(function(){ chrome.notifications.clear(id); }, isActive ? 2000 : 700);
+    });
+}
+
+//On browserAction click, or shortcut
+chrome.browserAction.onClicked.addListener(function() {
+    toggleMe();
+});
+chrome.commands.onCommand.addListener(function (command) {
+    if (command === "toggle") {
+        toggleMe();
+    }
 });
 
 //On new opened tab
