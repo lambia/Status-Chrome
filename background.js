b34fdbf7955d4daea3930b4a8b3098c97c34f38f
Luca <luca@lambia.it>
2017-09-21 00:19:28 +0200

    Minor changes
    Bugfix for browser exiting

--- background.js
+++ background.js
@@ -7,14 +7,17 @@ fallback for notifications (if !permissions or !compatibility, go with alert)
 notification onclick -> clear(id)
 test the background.dev.js code for js injection in page
 mute and wait for complete load of the tab before closing it
+FIX PROBLEM WITH CHROME CANARY!!!
+check mobile
 */
 
 /*
 CHANGELOG:
 Shortcut to toggle protection (ALT + SHIFT + S)
 Removed trash code (comments, dev stuff, old test)
-Fixed bug for the icon change just on the active tab
+Bugfix: the icon changed just on the active tab
 Added notification toast instead of alert
+Bugfix: turn off the protection automatically on browser exiting
 */
 
 isActive = false;
@@ -34,7 +37,7 @@ function toggleMe(){
     });
 }
 
-//On browserAction click, or shortcut
+//On browserAction click, or keyboard shortcut, or browser closing
 chrome.browserAction.onClicked.addListener(function() {
     toggleMe();
 });
@@ -42,6 +45,14 @@ chrome.commands.onCommand.addListener(function (command) {
     if (command === "toggle") {
         toggleMe();
     }
+});
+//Prevent the extension from blocking new windows, if the last was closed while the extension still on
+chrome.windows.onRemoved.addListener(function(windowid) {
+    chrome.windows.getAll({populate : true}, function (window_list) {
+        if(window_list<1){
+            toggleMe();
+        }
+    });
 });
 
 //On new opened tab
